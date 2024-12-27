853ea3364dbf5e33d0821055c667bcb20060c29c
yjgaia <yjdev88@gmail.com>
2024-12-27 13:18:18 +0900

    Refactor AssetManager to improve asset loading and management

--- lib/AssetManager.d.ts
+++ lib/AssetManager.d.ts
@@ -1,16 +1,22 @@
 declare class AssetManager {
-    load(...assets: ({
-        id: string;
-        src: string;
-        type?: "image" | "audio";
-    } | {
-        id: string;
-        atlas: string;
-        skel?: string;
-        json?: string;
-        png: Record<string, string> | string;
-        type?: "spine";
-    })[]): Promise<void>;
+    private assets;
+    load(sources: {
+        [id: string]: {
+            type: "image" | "audio";
+            src: string;
+        } | {
+            type: "spine";
+            atlas: string;
+            skel?: string;
+            json?: string;
+            png: Record<string, string> | string;
+        };
+    }): Promise<void>;
+    private loadImage;
+    private loadAudio;
+    private loadSpine;
+    private loadText;
+    private loadBinary;
 }
 declare const _default: AssetManager;
 export default _default;
--- src/AssetManager.ts
+++ src/AssetManager.ts
@@ -1,17 +1,144 @@
+import {
+  AtlasAttachmentLoader,
+  SkeletonBinary,
+  SkeletonData,
+  SkeletonJson,
+  SpineTexture,
+  TextureAtlas,
+} from "@pixi/spine-pixi";
+import { CanvasSource, Texture } from "pixi.js";
+
+const audioContext =
+  new (window.AudioContext || (window as any).webkitAudioContext)();
+["mousedown", "touchend"].forEach((event) =>
+  window.addEventListener(event, () => audioContext.resume())
+);
+
 class AssetManager {
-  public async load(
-    ...assets: (
-      | { id: string; src: string; type?: "image" | "audio" }
-      | {
-        id: string;
-        atlas: string;
-        skel?: string;
-        json?: string;
-        png: Record<string, string> | string;
-        type?: "spine";
+  private assets: Record<string, Texture | AudioBuffer | SkeletonData> = {};
+
+  public async load(sources: {
+    [id: string]: { type: "image" | "audio"; src: string } | {
+      type: "spine";
+      atlas: string;
+      skel?: string;
+      json?: string;
+      png: Record<string, string> | string;
+    };
+  }) {
+    const promises = Object.entries(sources).map(async ([id, source]) => {
+      if (this.assets[id]) {
+        throw new Error(`Asset already exists: ${id}`);
       }
-    )[]
-  ) {
+      if (source.type === "image") {
+        this.assets[id] = await this.loadImage(source.src);
+      } else if (source.type === "audio") {
+        this.assets[id] = await this.loadAudio(source.src);
+      } else if (source.type === "spine") {
+        this.assets[id] = await this.loadSpine(source);
+      }
+    });
+    await Promise.all(promises);
+  }
+
+  private async loadImage(src: string) {
+    return new Promise<Texture>((resolve, reject) => {
+      const image = new Image();
+      image.src = src;
+      image.crossOrigin = "anonymous";
+
+      image.onload = () => {
+        const canvas = document.createElement("canvas");
+        canvas.width = image.width;
+        canvas.height = image.height;
+
+        const context = canvas.getContext("2d");
+        if (!context) throw new Error("Failed to get 2d context");
+
+        context.drawImage(image, 0, 0);
+
+        const source = new CanvasSource({ resource: canvas });
+        resolve(new Texture({ source }));
+
+        canvas.remove();
+      };
+
+      image.onerror = (event) => {
+        console.error(`Failed to load texture: ${src}`);
+        reject(event);
+      };
+    });
+  }
+
+  private async loadAudio(src: string) {
+    const response = await fetch(src);
+    const arrayBuffer = await response.arrayBuffer();
+    return audioContext.decodeAudioData(arrayBuffer);
+  }
+
+  private async loadSpine(source: {
+    atlas: string;
+    skel?: string;
+    json?: string;
+    png: Record<string, string> | string;
+  }) {
+    const textAtlasData = await this.loadText(source.atlas);
+    let skeletonBynary: Uint8Array | undefined;
+    let textSkeletonData: string | undefined;
+
+    if (source.skel !== undefined) {
+      skeletonBynary = await this.loadBinary(source.skel);
+    } else if (source.json !== undefined) {
+      textSkeletonData = await this.loadText(source.json);
+    } else {
+      throw new Error("Either skel or json must be provided");
+    }
+
+    let texture: Texture | undefined;
+    let textures: Record<string, Texture> | undefined;
+
+    if (typeof source.png === "string") {
+      texture = await this.loadImage(source.png);
+    } else {
+      textures = {};
+      for (const [key, path] of Object.entries(source.png)) {
+        textures[key] = await this.loadImage(path);
+      }
+    }
+
+    const atlas = new TextureAtlas(textAtlasData);
+    atlas.pages.forEach((page) => {
+      if (texture) page.setTexture(SpineTexture.from(texture.source));
+      else if (textures) {
+        page.setTexture(SpineTexture.from(textures[page.name].source));
+      }
+    });
+
+    const atlasLoader = new AtlasAttachmentLoader(atlas);
+
+    let skeletonData: SkeletonData;
+    if (skeletonBynary) {
+      const binaryLoader = new SkeletonBinary(atlasLoader);
+      skeletonData = binaryLoader.readSkeletonData(skeletonBynary);
+    } else if (textSkeletonData) {
+      const jsonLoader = new SkeletonJson(atlasLoader);
+      skeletonData = jsonLoader.readSkeletonData(textSkeletonData);
+    } else {
+      throw new Error("Either skel or json must be provided");
+    }
+
+    return skeletonData;
+  }
+
+  private async loadText(src: string) {
+    const response = await fetch(src);
+    return await response.text();
+  }
+
+  private async loadBinary(src: string) {
+    const response = await fetch(src);
+    const arrayBuffer = await response.arrayBuffer();
+    return new Uint8Array(arrayBuffer);
   }
 }
 
